12c8a2d49992826fed2828750d635b9c7ca1f841
shaik sohail <shaik16sohail@gmail.com>
2024-10-19 22:37:13 +0530

    completed landing page

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,78 +1,146 @@
 import React from 'react'
-function createTicket() {
-    return (  
-        <div className='container mt-5'>
-        <div className='row  mt-5  mb-5'>
-          <p className='fs-5 text-muted'>
+function createTicket () {
+  return (
+    <div className='container mt-5'>
+      <div className='row  mt-5  mb-5'>
+        <p className='fs-5 text-muted'>
           To create a ticket, select a relevant topic
+        </p>
+        <div className='col ms-4'>
+          <p className='fs-6'>
+            <i class='fa-solid fa-circle-plus fa-1x'></i> Account Opening
           </p>
-          <div className='col ms-4'>
-            <p className='fs-6'><i class="fa-solid fa-circle-plus fa-1x"></i> Account Opening</p>
-            <div style={{lineHeight:"0.9"}} className='ms-2 text-muted'>
-                <p className='fs-8'><a href='#'>Online</a></p>
-                <p className='fs-8'><a href='#'>Offline</a></p>
-                <p className='fs-8'><a href='#'>Charges</a></p>
-                <p className='fs-8'><a href='#'>Company, Partnership and HUF</a></p>
-                <p className='fs-8'><a href='#'>Non Resident Indian (NRI)</a></p>
-            </div>
-          </div>
-          <div className='col'>
-          <p className='fs-6'>Your Zerodha Account</p>
-            <div style={{lineHeight:"0.9"}} className='ms-2 text-muted'>
-                <p className='fs-8'><a href='#'> Login credentials</a></p>
-                <p className='fs-8'><a href='#'>Your Profile</a></p>
-                <p className='fs-8'><a href='#'>Account modification and segment addition</a></p>
-                <p className='fs-8'><a href='#'>Nomination</a></p>
-                <p className='fs-8'><a href='#'>Transfer and conversion of shares</a></p>
-            </div>
-          </div>
-          <div className='col'>
-          <p className='fs-6'><i class="fa-solid fa-chart-simple"></i> Trading and Markets</p>
-            <div style={{lineHeight:"0.9"}} className='ms-2 text-muted'>
-                <p className='fs-8'><a href='#'>Trading FAQs</a></p>
-                <p className='fs-8'><a href='#'>Kite</a></p>
-                <p className='fs-8'><a href='#'>Margins</a></p>
-                <p className='fs-8'><a href='#'>Product and order types</a></p>
-                <p className='fs-8'><a href='#'>Corporate actions</a></p>
-                <p className='fs-8'><a href='#'>Kite features</a></p>
-            </div>
+          <div style={{ lineHeight: '0.9' }} className='ms-2 text-muted'>
+            <p className='fs-8'>
+              <a href='#'>Online</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Offline</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Charges</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Company, Partnership and HUF</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Non Resident Indian (NRI)</a>
+            </p>
           </div>
         </div>
-        <div className='row mb-5 ms-2 '>
-          <div className='col'>
-            <p className='fs-6'>Account Opening</p>
-            <div style={{lineHeight:"0.9"}} className='ms-2 text-muted'>
-                <p className='fs-8'><a href='#'>Online</a></p>
-                <p className='fs-8'><a href='#'>Offline</a></p>
-                <p className='fs-8'><a href='#'>Charges</a></p>
-                <p className='fs-8'><a href='#'>Company, Partnership and HUF</a></p>
-                <p className='fs-8'><a href='#'>Non Resident Indian (NRI)</a></p>
-            </div>
-          </div>
-          <div className='col ms-4'>
+        <div className='col'>
           <p className='fs-6'>Your Zerodha Account</p>
-            <div style={{lineHeight:"0.9"}} className='ms-2 text-muted'>
-                <p className='fs-8'><a href='#'> Login credentials</a></p>
-                <p className='fs-8'><a href='#'>Your Profile</a></p>
-                <p className='fs-8'><a href='#'>Account modification and segment addition</a></p>
-                <p className='fs-8'><a href='#'>Nomination</a></p>
-                <p className='fs-8'><a href='#'>Transfer and conversion of shares</a></p>
-            </div>
+          <div style={{ lineHeight: '0.9' }} className='ms-2 text-muted'>
+            <p className='fs-8'>
+              <a href='#'> Login credentials</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Your Profile</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Account modification and segment addition</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Nomination</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Transfer and conversion of shares</a>
+            </p>
           </div>
-          <div className='col'>
+        </div>
+        <div className='col'>
+          <p className='fs-6'>
+            <i class='fa-solid fa-chart-simple'></i> Trading and Markets
+          </p>
+          <div style={{ lineHeight: '0.9' }} className='ms-2 text-muted'>
+            <p className='fs-8'>
+              <a href='#'>Trading FAQs</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Kite</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Margins</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Product and order types</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Corporate actions</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Kite features</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='row mb-5 ms-2 '>
+        <div className='col'>
+          <p className='fs-6'>Account Opening</p>
+          <div style={{ lineHeight: '0.9' }} className='ms-2 text-muted'>
+            <p className='fs-8'>
+              <a href='#'>Online</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Offline</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Charges</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Company, Partnership and HUF</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Non Resident Indian (NRI)</a>
+            </p>
+          </div>
+        </div>
+        <div className='col ms-4'>
+          <p className='fs-6'>Your Zerodha Account</p>
+          <div style={{ lineHeight: '0.9' }} className='ms-2 text-muted'>
+            <p className='fs-8'>
+              <a href='#'> Login credentials</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Your Profile</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Account modification and segment addition</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Nomination</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Transfer and conversion of shares</a>
+            </p>
+          </div>
+        </div>
+        <div className='col'>
           <p className='fs-6'>Trading and Markets</p>
-            <div style={{lineHeight:"0.9"}} className='ms-2 text-muted'>
-                <p className='fs-8'><a href='#'>Trading FAQs</a></p>
-                <p className='fs-8'><a href='#'>Kite</a></p>
-                <p className='fs-8'><a href='#'>Margins</a></p>
-                <p className='fs-8'><a href='#'>Product and order types</a></p>
-                <p className='fs-8'><a href='#'>Corporate actions</a></p>
-                <p className='fs-8'><a href='#'>Kite features</a></p>
-            </div>
+          <div style={{ lineHeight: '0.9' }} className='ms-2 text-muted'>
+            <p className='fs-8'>
+              <a href='#'>Trading FAQs</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Kite</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Margins</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Product and order types</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Corporate actions</a>
+            </p>
+            <p className='fs-8'>
+              <a href='#'>Kite features</a>
+            </p>
           </div>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  )
 }
 
-export default createTicket;
+export default createTicket
--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,12 +1,46 @@
 import React from 'react'
 
+function Hero () {
+  return (
+    <div
+      style={{ backgroundColor: '#387ed1', color: '#fff' }}
+      className='supportHero'
+    >
+      <div className='container '>
+        <div className='  ' id='supportWrapper'>
+          <h5>Support Portol</h5>
+          <a href='#' style={{ color: '#fff', textDecoration: 'underline' }}>
+            Track Tickets
+          </a>
+        </div>
+        <div className='row '>
+          <div className='col p-5 supportInput '>
+            <p className='fs-6'>
+              Search for an answer or browse help topics to create a ticket
+            </p>
+            <input placeholder='Eg: how do i activate F&O,why is my order getting rejected...' />
+            <br /><br></br>
+            <a href='#' class="underline">Track account opening</a> &nbsp; 
+<a href='#' class="underline">Track segment</a>&nbsp;&nbsp;
+<a href='#' class="underline">Intraday margins</a>&nbsp;&nbsp;
+<a href='#' class="underline">User manual</a>
 
-function Hero() {
-    return ( 
-        <>
-            <h1>Hero</h1>
-        </>
-     );
+          </div>
+          <div className='col p-5 ms-5'>
+            <p className='fs-5'>Featured</p>
+            <ol>
+              <li>
+                <a href='#'>Current Takeovers and Delisting - January</a>
+              </li>
+              <li>
+                <a href='#'>Latest Intraday leverages -MIS & CO</a>{' '}
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Hero;
+export default Hero
