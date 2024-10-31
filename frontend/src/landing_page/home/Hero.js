c20a1e9d71498fc5a21195cc509a1d3f001832b0
shaik sohail <shaik16sohail@gmail.com>
2024-10-31 17:51:40 +0530

    small change

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 
 export default function Hero(){
     return (
@@ -8,7 +8,7 @@ export default function Hero(){
                 <img src='media/images/homeHero.png' alt='Hero_Image' className='mb-5'></img>
                 <h1 className='mt-5'>Invest in everything</h1>
                 <p>Online platform to invest in stocks,derivates,mutual funds, and more</p>
-                <button className='mt-4 p-1 btn btn-primary fs-5' style={{width:"23%",margin:"0 auto"}}>Signup Now</button>
+                <button className='mt-4 p-1 btn btn-primary fs-5' style={{width:"23%",margin:"0 auto"}}><Link style={{color:'white'}} to={"/signup"}>Signup Now</Link></button>
             </div>
         </div>
     );
